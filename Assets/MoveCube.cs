f76c753982eb2b0de727f76cb7602e0f9400aa17
Sebastian Vråle <seb@terminator.net>
2020-03-23 14:00:08 +0100

    Cube burde bevege seg med 1f speed

--- Assets/MoveCube.cs
+++ Assets/MoveCube.cs
@@ -7,12 +7,12 @@ public class MoveCube : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("Hei, jeg er MoveCube");
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.position = transform.position + new Vector3(0, Time.deltaTime * 0.5f, 0);
+        transform.position = transform.position + new Vector3(0, Time.deltaTime * 1f, 0);
     }
 }
